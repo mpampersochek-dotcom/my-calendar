8e9697ff4eabaf34132a0c954e65857bb008d43e
mpampersochek-dotcom <m.pampersochek@gmail.com>
2026-09-08 00:41:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE = 'moy-kalendar-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE = 'my-calendar-v2';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './apple-touch-icon.png'
+];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
@@ -8,9 +11,11 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-    const copy = resp.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
-    return resp;
-  }).catch(() => caches.match('./index.html'))));
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      if (event.request.url.startsWith(self.location.origin)) caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
